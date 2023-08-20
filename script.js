5b4008ba0a1fc21e9278856788ee476bdd3b6935
anacarolinabmv <141560459+anacarolinabmv@users.noreply.github.com>
2023-08-20 20:50:56 +0200

    Refactored JS code

--- script.js
+++ script.js
@@ -8,11 +8,9 @@ const customTipEl = document.getElementById('custom');
 const tipAmountEl = document.getElementById('tipAmount');
 const totalEl = document.getElementById('total');
 const btnReset = document.getElementById('btn--reset');
-
-let tip;
+const errorLabel = document.getElementById('error');
 
 const init = function () {
-  tip = 0;
   billEl.value = '';
   customTipEl.value = '';
   peopleEl.value = '';
@@ -22,30 +20,67 @@ const init = function () {
 };
 init();
 
+const getTip = function () {
+  const arr = Array.from(btnsTips);
+  const btnTip = arr.find((btn) => btn.classList.contains('btn--pressed'));
+
+  let tip = btnTip ? +btnTip.firstChild.textContent : +customTipEl.value;
+
+  if (!tip) tip = 0;
+
+  return tip;
+};
+
+//Add background color to pressed button
 btnsTipContainer.addEventListener('click', function (event) {
   btnsTips.forEach((btn) => btn.classList.remove('btn--pressed'));
+
   const target = event.target.closest('button');
   if (!target) return;
   target.classList.add('btn--pressed');
 
-  tip = +target.querySelector('#tip--pctg').textContent;
+  getTip();
 });
 
-const calcTip = function (event) {
+//Calc and render tip and bill amounts
+const calcTotal = function () {
+  const tip = getTip();
   const bill = +billEl.value;
   const people = +peopleEl.value;
+
   const totalTip = (bill * tip) / 100;
-  const tipPerson = Math.round((totalTip / people) * 100) / 100;
-  const totalPerson = ((bill / people) * 100) / 100 + tipPerson;
+  const billPerson = +(bill / people).toFixed(2);
+  const tipPerson = +(totalTip / people).toFixed(2);
+  const totalPerson = +(tipPerson + billPerson).toFixed(2);
 
   tipAmountEl.textContent = tipPerson;
-  totalEl.textContent = Math.round(totalPerson * 100) / 100;
+  totalEl.textContent = totalPerson;
 };
 
+//Show and restore errors
+const renderError = function () {
+  errorLabel.classList.add('display--error');
+  peopleEl.classList.add('error--border');
+};
+
+const restoreError = function () {
+  errorLabel.classList.remove('display--error');
+  peopleEl.classList.remove('error--border');
+};
+
+peopleEl.addEventListener('click', restoreError);
+
+//Submit form on Enter
 peopleEl.addEventListener('keydown', (event) => {
   if (event.key !== 'Enter') return;
-  if (customTipEl.value) tip = +customTipEl.value;
-  calcTip(event);
+
+  if (!peopleEl.value) {
+    renderError();
+    return;
+  }
+
+  calcTotal();
 });
 
+//Restore default UI
 btnReset.addEventListener('click', init);
